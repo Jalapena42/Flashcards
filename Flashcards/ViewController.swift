40b1ecbde60b8a5a1b097824e2ccd8101ac23dfd
Bad Mothafucka <reecepena@reeces-macbook-pro.local>
2018-10-20 14:59:47 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Reece Pena on 10/20/18.
+//  Copyright © 2018 jalapena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
